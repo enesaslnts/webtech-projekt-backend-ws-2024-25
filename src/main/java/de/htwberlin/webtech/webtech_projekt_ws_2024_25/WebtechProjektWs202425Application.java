4eb2547c856a4f60e9024f6a48555a85e9b7e467
enesaslnts <Enes.Aslantas@Student.HTW-Berlin.de>
2024-10-17 18:45:46 +0200

    Projektstart auf Git pushen

---
+++ src/main/java/de/htwberlin/webtech/webtech_projekt_ws_2024_25/WebtechProjektWs202425Application.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.webtech_projekt_ws_2024_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechProjektWs202425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechProjektWs202425Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/webtech_projekt_ws_2024_25/WebtechProjektWs202425ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.webtech_projekt_ws_2024_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjektWs202425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
